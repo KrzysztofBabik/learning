96101a0dfe22a444af3ac56152dc5f101e125dd2
Krzysiek <krzysztofbabik.kb@gmail.com>
2019-04-30 16:03:54 +0200

    Init commit

---
+++ ModifierExample/ModifierExample/StopwatchControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class StopwatchControl
+    {
+        public TimeSpan StopwatchControll()
+        {
+            var stopwatch = new StopwatchStartStop();
+
+            var start = stopwatch.Start();
+            var stop = stopwatch.Stop();
+
+            var interval = stop - start;
+
+            Console.WriteLine("The duration between start and stop is: {0}", interval);
+            return interval;
+        }
+
+        public void StopwatchResults()
+        {
+            var results = new List<TimeSpan>();
+            var test = 'y';
+
+            while (test != 'n')
+            {
+                var result = StopwatchControll();
+                results.Add(result);
+
+                Console.WriteLine("Do you want to use Stopwatch again? y/n");
+                test = Convert.ToChar(Console.ReadLine());
+
+                Console.Clear();
+            }
+
+            foreach(var result in results)
+                Console.WriteLine("{0} result: {1}", results.IndexOf(result)+1, result);
+        }
+    }
+}
---
+++ ModifierExample/ModifierExample/StopwatchStartStop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class StopwatchStartStop
+    {
+        public DateTime Start()
+        {
+            Console.WriteLine("Press any key to start the stopwatch.");
+            Console.ReadKey();
+            var StartTime = DateTime.Now;
+            return StartTime;
+        }
+
+        public DateTime Stop()
+        {
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadKey();
+            var StopTime = DateTime.Now;
+            return StopTime;
+        }
+    }
+}
